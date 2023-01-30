f57ec02f872aaf7da789188e9feb5df2d2f85d15
ГришанинаКИ <@106-03>
2023-01-30 14:57:36 +0300

    Добавьте файлы проекта.

---
+++ Auten_2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Auten_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string kod;
+        public MainWindow()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            String log = "World";
+            String pass = "1023";
+
+            if ((Login.Text == log)&&(Pass.Text == pass))
+            { 
+
+                Btn.IsEnabled = false;
+
+                Random x = new Random();
+                int n = x.Next(10000, 99999);
+                kod = n.ToString();
+                MessageBox.Show(Convert.ToString(n));
+
+                Window1 winUser = new Window1();
+                winUser.ShowDialog();
+                //Close(); 
+               
+                st1.Visibility = Visibility.Visible;
+
+            }
+            else 
+            { 
+                
+            }
+        }
+    }
+}
---
+++ Auten_2/Window1.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Auten_2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        DispatcherTimer disTimer = new DispatcherTimer();
+        public Window1()
+        {
+            InitializeComponent();
+            disTimer.Interval = new TimeSpan(0, 0, 5);
+            disTimer.Tick += new EventHandler(DisTimer_Tick);
+            disTimer.Start();
+            Time_Kode.Text = disTimer.ToString();
+        }
+
+        private void bb_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if(Kod.Text==MainWindow.kod) // Значение переменной kod из окна MainWindow
+            {
+                MessageBox.Show("Авторизация прошла \nуспешно");
+            }
+            else
+            {
+                MessageBox.Show("ОШИБКА!!!");
+            }
+        }
+
+        private void DisTimer_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Время вышло!");
+            //MainWindow mainwindow = new MainWindow();
+            //mainwindow.Show();
+
+            //MessageBox.Show("ВРЕМЯ ВЫШЛО!");
+            this.Close();
+            
+        }
+    }
+}
